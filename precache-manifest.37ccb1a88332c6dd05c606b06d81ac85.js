c975692bf6f8bbb80c70ea40fa933dbc6cf7d794
Pamcy <chinyi.pam@gmail.com>
2019-10-05 16:40:42 +0800

    Updates

---
+++ precache-manifest.37ccb1a88332c6dd05c606b06d81ac85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efb9f487b95569048d7c4a786c13dbd",
+    "url": "/react-hotel-booking/index.html"
+  },
+  {
+    "revision": "9690f2645e0698b299a3",
+    "url": "/react-hotel-booking/static/css/2.9f3ac81c.chunk.css"
+  },
+  {
+    "revision": "a93ed20ebd172803b9e1",
+    "url": "/react-hotel-booking/static/css/main.da026b09.chunk.css"
+  },
+  {
+    "revision": "9690f2645e0698b299a3",
+    "url": "/react-hotel-booking/static/js/2.b478bc34.chunk.js"
+  },
+  {
+    "revision": "a93ed20ebd172803b9e1",
+    "url": "/react-hotel-booking/static/js/main.3fe71c80.chunk.js"
+  },
+  {
+    "revision": "01abf972912578ec4655",
+    "url": "/react-hotel-booking/static/js/runtime~main.7779c284.js"
+  }
+]);

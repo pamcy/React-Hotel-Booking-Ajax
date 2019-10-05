bd3e94c254a8149fd5a5bac59c2f1421aa4ba9a7
Pamcy <chinyi.pam@gmail.com>
2019-10-05 14:31:26 +0800

    :chart_with_upwards_trend: Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import Router from './components/Router';
 import './base-styles/index.scss';
 
+Sentry.init({ dsn: 'https://7dcec06e0dbb4e32ad7ab74a4d614723@sentry.io/1770835' });
 ReactDOM.render(<Router />, document.getElementById('root'));

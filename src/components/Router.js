32e1dcfd5d99af8ac9a609d443128d4a9ed76f5a
Pamcy <chinyi.pam@gmail.com>
2019-10-05 14:11:38 +0800

    :sparkles: Add Github Corner

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import GithubCorner from 'react-github-corner';
+
 import MainPage from '../pages/MainPage/index';
 import DetailsPage from '../pages/DetailsPage/index';
 
 const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/room/:roomId" component={DetailsPage} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/room/:roomId" component={DetailsPage} />
+      </Switch>
+    </BrowserRouter>
+    <GithubCorner
+      style={{ position: 'fixed', top: 0, right: 0, zIndex: 1 }}
+      direction="right"
+      href=""
+      target="_blank"
+    />
+  </>
 );
 
 export default Router;

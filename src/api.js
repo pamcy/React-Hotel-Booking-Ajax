4a02994a901b63b5c33f61cd0e5f30b5abb1b602
Pamcy <chinyi.pam@gmail.com>
2019-10-06 08:20:50 +0800

    Fix Netlify 401 error

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const roomRequest = axios.create({
   baseURL: 'https://challenge.thef2e.com/api/thef2e2019/stage6/',
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+    Authorization: 'Bearer IgFPqVrgQLEc0eyA4qqxgYXjg667Dz1C9gwsC7SowmQRfFTuJEBgFrrjs9rG',
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
